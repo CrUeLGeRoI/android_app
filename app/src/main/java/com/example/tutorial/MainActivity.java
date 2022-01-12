29848ed271c9763f9778d7dd5baf1269571a3705
CrUeLGeRoI <elbg2016@gmail.com>
2022-01-12 13:22:01 +0200

    Another activity

--- app/src/main/java/com/example/tutorial/MainActivity.java
+++ app/src/main/java/com/example/tutorial/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,19 +14,27 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private RelativeLayout currentLayout;
+    private Button button1, button2, button3, button4, button5, button6, button7, button8, transition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextViews
         textView = findViewById(R.id.hello_world);
-        Button button1 = findViewById(R.id.button);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
+
+        //Buttons
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        transition = findViewById(R.id.transition_button);
+
+        //Layouts
         currentLayout = findViewById(R.id.main_layout);
 
         button1.setOnClickListener(new View.OnClickListener() {
@@ -100,7 +109,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        transition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(".FileActivity");
+                startActivity(intent);
+            }
+        });
+
+
+
+        //end
     }
+
 
     public void reset(View view) {
         currentLayout = (RelativeLayout) findViewById(R.id.main_layout);
